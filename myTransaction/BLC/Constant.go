eee3259f8a66d468d98a65b907871878245d6ada
urnotcoward <1254297167@qq.com>
2021-03-14 17:01:03 +0800

    UTXO_test

---
+++ myTransaction/BLC/Constant.go
@@ -0,0 +1,3 @@
+package BLC
+const DBNAME = "blockchain.db"  //数据库名
+const BLOCKTABLENAME = "blocks" //表名
---
+++ myTransaction/BLC/Transaction_TxOutput.go
@@ -0,0 +1,11 @@
+package BLC
+type TXOuput struct {
+	Value        int64
+	//一个锁定脚本(ScriptPubKey)，要花这笔钱，必须要解锁该脚本。
+	ScriptPubKey string //公钥：先理解为，用户名
+}
+
+//判断当前txOutput消费，和指定的address是否一致
+func (txOutput *TXOuput) UnLockWithAddress(address string) bool{
+	return txOutput.ScriptPubKey == address
+}
